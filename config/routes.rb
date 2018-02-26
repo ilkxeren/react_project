4ba74dc177e970ad232e6d68227d1c560669ec04
“Ilknur <ilkxeren@gmail.com>
2018-02-25 21:29:13 -0500

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  
+  get "/cars" => "cars#index"
+  get "/cars/:id" => "cars#show"
+  post "/cars" => "cars#create"
+  patch "/cars/:id" => "cars#update"
+  delete "/cars/:id" => "cars#destroy"
+end
