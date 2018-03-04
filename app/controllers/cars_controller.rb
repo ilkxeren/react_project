2166f6d471646b32b55c22561034298d8dbb0d5b
“Ilknur <ilkxeren@gmail.com>
2018-03-03 23:05:50 -0500

    my server is not working. Maybe this will help

---
+++ app/controllers/cars_controller.rb
@@ -0,0 +1,38 @@
+class CarsController < ApplicationController
+
+  def index
+    cars = Car.all
+    render json: cars.as_json
+  end
+
+  def create
+    car = Car.new(
+      year: params[:year],
+      model: params[:model]
+      )
+    car.save!
+    render json: car.as_json
+  end
+
+  def update
+    car = Car.find_by(id: params[:id])
+
+    car.update!(
+    year: params[:year],
+    model: params[:model]
+    )
+    render json: car.as_json
+  end
+
+  def show
+    car = Car.find_by(id: params[:id])
+    render json: car.as_json
+  end
+
+  def destroy
+    car = Car.find_by(id: params[:id])
+    car.delete
+    render json: {message: "You deleted this"}
+  end
+
+end
